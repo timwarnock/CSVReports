25aee41a31a85724490ae363d965f187c9d0805f
Timothy Warnock <tim@timwarnock.com>
2012-04-24 15:20:13 -0400

    slice.stop

--- Reports.py
+++ Reports.py
@@ -135,7 +135,7 @@ class Report(collections.Mapping):
 			if key.step is not None:
 				qstep = ' AND ROWID %% %s = %s ' %(key.step, start+1 % key.step)
 			res = curs.execute('SELECT * FROM %s WHERE ROWID >= %s AND ROWID <= %s %s' 
-				%(self.table, start+1, stop - start, qstep)).fetchall()
+				%(self.table, start+1, stop, qstep)).fetchall()
 		else:
 			res = list(curs.execute('SELECT * FROM %s WHERE ROWID = %s' %(self.table, key+1)).fetchall()[0])
 		curs.close()
